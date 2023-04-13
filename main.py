83fea07ca995c03a4afe839f51a5e58e7dba920d
baiv84 <iambabintsev@gmail.com>
2023-04-13 17:12:57 +0700

    Add template + calculate age automatically

--- main.py
+++ main.py
@@ -1,5 +1,43 @@
-from http.server import HTTPServer, SimpleHTTPRequestHandler
+import datetime
+from http.server import HTTPServer
+from http.server import SimpleHTTPRequestHandler
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+def get_year_tizer(year):
+    """Calculate year tizer"""
+    tizers = ['лет', 'года', 'год']
+    k = (year // 10 % 10 != 1) * (year % 10)
+    return tizers[(k == 1) + (1 <= k <= 4)]
+
+
+def calulate_winery_age():
+    """Calculate winery age"""
+    age = (datetime.date.today().year - 1920) 
+    age_string = get_year_tizer(age)
+    return age, age_string
+
+
+def main():
+    """Program entry point"""
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+
+    template = env.get_template('template.html')
+    winery_age, age_tizer = calulate_winery_age()
+    
+    rendered_page = template.render(
+        winery_age = winery_age,
+        age_tizer = age_tizer
+    )
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+
+if __name__=='__main__':
+    main()

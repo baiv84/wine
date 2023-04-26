7302b57670d4eab87a4a3a337738950f46c7638e
baiv84 <iambabintsev@gmail.com>
2023-04-27 00:50:21 +0700

    Rename data example file

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import os
 import pandas
 import datetime
 import collections
 from http.server import HTTPServer
 from http.server import SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+from dotenv import load_dotenv
 
 WINERY_FOUNDATION_YEAR = 1920
 
@@ -49,6 +51,13 @@ def load_wine_by_categories(file_name='wine.xlsx'):
 
 def main():
     """Program entry point"""
+    dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+    if not os.path.exists(dotenv_path):
+        raise
+
+    load_dotenv(dotenv_path)
+    datafile = os.environ.get("DATAFILE")
+
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
@@ -56,7 +65,7 @@ def main():
     template = env.get_template('template.html')
     winery_age = calulate_winery_age()
     winery_age_tizer = get_year_tizer(winery_age)
-    wine_database = load_wine_by_categories(file_name='database.xlsx')
+    wine_database = load_wine_by_categories(file_name=datafile)
 
     rendered_page = template.render(wine_database=wine_database,
                                     winery_age=winery_age,
